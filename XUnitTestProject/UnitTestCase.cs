4354b76ca30c8d6007d3871ba08297c863e93385
rajibmu <rajibtestvb@outlook.com>
2020-07-09 06:35:13 +0530

    Test with different options updated and success

--- XUnitTestProject/UnitTestCase.cs
+++ XUnitTestProject/UnitTestCase.cs
@@ -15,11 +15,11 @@ namespace XUnitTestProject
             var promotions = new List<Promotion>() { new Promotion(1, new List<char> { 'A' }, 3, 130)
                 , new Promotion(1, new List<char> { 'B' }, 2, 45)
                 , new Promotion(2, new List<char> { 'C', 'D' }, 0, 30) };
-            var cart = new Dictionary<char, int> { { 'A', 5 }, { 'B', 5 }, { 'C', 1 }, { 'D', 1 } };
+            var cart = new Dictionary<char, int> { { 'A', 5 }, { 'B', 5 }, { 'C', 2 }, { 'D', 2 } };
 
             var actual = PromoEngine.Calculate(items, promotions, cart);
             
-            int expected = 380;
+            int expected = 410;
             Assert.Equal(expected, actual);
         }
     }
