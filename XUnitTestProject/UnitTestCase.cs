8219a8d90079a4892ab7559af529ba52877d70df
rajibmu <rajibtestvb@outlook.com>
2020-07-09 06:24:06 +0530

    Test different cases with added promotion n items.

--- XUnitTestProject/UnitTestCase.cs
+++ XUnitTestProject/UnitTestCase.cs
@@ -12,12 +12,12 @@ namespace XUnitTestProject
         public void Test_PromoEngine_Calculate()
         {
             var items = new List<Item>() { new Item('A', 50), new Item('B', 30), new Item('C', 20), new Item('D', 15) };
-            var promotions = new List<Promotion>() { new Promotion(1, new List<char> { 'A' }, 3, 130) };
-            var cart = new Dictionary<char, int> { { 'A', 3 }, { 'B', 1 }, { 'C', 1 }, { 'D', 1 } };
+            var promotions = new List<Promotion>() { new Promotion(1, new List<char> { 'A' }, 3, 130), new Promotion(1, new List<char> { 'B' }, 2, 45)};
+            var cart = new Dictionary<char, int> { { 'A', 5 }, { 'B', 5 }, { 'C', 1 } };
 
             var actual = PromoEngine.Calculate(items, promotions, cart);
             
-            int expected = 195;
+            int expected = 370;
             Assert.Equal(expected, actual);
         }
     }
