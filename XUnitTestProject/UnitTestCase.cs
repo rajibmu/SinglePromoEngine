db7d955e371e4588b9737e13efc7b9a218bd008d
rajibmu <rajibtestvb@outlook.com>
2020-07-09 07:56:47 +0530

    Multiple Ptromo applied

--- XUnitTestProject/UnitTestCase.cs
+++ XUnitTestProject/UnitTestCase.cs
@@ -16,7 +16,7 @@ namespace XUnitTestProject
                 , new Promotion(1, new List<char> { 'B' }, 2, 45)
                 , new Promotion(2, new List<char> { 'C', 'D' }, 0, 30) };
             //var promotions = new List<Promotion>() { new Promotion(1, new List<char> { 'A' }, 3, 130), new Promotion(3, new List<char> { 'A' }, 10, 0)};
-            var cart = new Dictionary<char, int> { { 'A', 3 } };
+            var cart = new Dictionary<char, int> { { 'A', 3 }, { 'B', 3 }, { 'C', 3 }, { 'D', 3 } };
 
             var actual = PromoEngine.Calculate(items, promotions, cart);
             
