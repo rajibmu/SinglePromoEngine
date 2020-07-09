8f4816887f0c07de11faa0185252443ac3c9f0a3
rajibmu <rajibtestvb@outlook.com>
2020-07-09 05:34:26 +0530

    Intialise Commit

---
+++ PromotionEngine/PromoEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PromotionEngine
+{
+    class PromoEngine
+    {
+        static void Main(string[] args)
+        {
+            PromoEngine.Calculate();
+        }
+
+        public static double Calculate()
+        {
+            double totalOrdersAmount = 0;
+
+            return totalOrdersAmount;
+        }
+}
