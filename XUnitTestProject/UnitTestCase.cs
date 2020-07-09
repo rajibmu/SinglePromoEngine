c8f76a3fbf9b415fa9775f2a31294955ef27b31a
rajibmu <rajibtestvb@outlook.com>
2020-07-09 07:54:54 +0530

    Test case Changes

--- XUnitTestProject/UnitTestCase.cs
+++ XUnitTestProject/UnitTestCase.cs
@@ -12,10 +12,10 @@ namespace XUnitTestProject
         public void Test_PromoEngine_Calculate()
         {
             var items = new List<Item>() { new Item('A', 50), new Item('B', 30), new Item('C', 20), new Item('D', 15) };
-            //var promotions = new List<Promotion>() { new Promotion(1, new List<char> { 'A' }, 3, 130)
-            //    , new Promotion(1, new List<char> { 'B' }, 2, 45)
-            //    , new Promotion(2, new List<char> { 'C', 'D' }, 0, 30) };
-            var promotions = new List<Promotion>() { new Promotion(1, new List<char> { 'A' }, 3, 130), new Promotion(3, new List<char> { 'A' }, 10, 0)};
+            var promotions = new List<Promotion>() { new Promotion(1, new List<char> { 'A' }, 3, 130)
+                , new Promotion(1, new List<char> { 'B' }, 2, 45)
+                , new Promotion(2, new List<char> { 'C', 'D' }, 0, 30) };
+            //var promotions = new List<Promotion>() { new Promotion(1, new List<char> { 'A' }, 3, 130), new Promotion(3, new List<char> { 'A' }, 10, 0)};
             var cart = new Dictionary<char, int> { { 'A', 3 } };
 
             var actual = PromoEngine.Calculate(items, promotions, cart);
